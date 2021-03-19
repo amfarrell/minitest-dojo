3ae384ae6929f97222a3c956d25b0310fce1eb30
Andrew M. Farrell <andrew.simplybusiness.co.uk>
2021-03-19 15:35:02 +0000

    Can add to hash of arrays

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # minitest-dojo
 
+## To Run
+ruby concordance_test.rb
+
 ## Learnings
 To fix the ruby error, gem update railties.
 Why? Dunno.
--- concordance_test.rb
+++ concordance_test.rb
@@ -1,19 +1,57 @@
 require 'minitest/autorun'
 
 class Concordance
-  def ask(text)
-    1
+  def ask(line)
+    words = Hash.new
+    line.each_with_index do |word, i|
+      if words.include?(word)
+        words[word].push(i+1)
+      else
+        words[word] = [i+1]
+      end
+    end
+    words
   end
 end
 
 class ConcordanceTest < Minitest::Test
-  def test_ask_returns_an_answer
-    concordance = Concordance.new
-    assert concordance.ask("Whatever") != nil
-  end
-
   def test_counts_hash_for_a_single_word
     concordance = Concordance.new
-    assert_equal {"Whatever" => 1}, concordance.ask("Whatever")
+    assert_equal({"Whatever" => [1]}, concordance.ask(["Whatever"]))
+  end
+
+  def test_lists_all_the_lines_for_a_single_word
+    concordance = Concordance.new
+    lines = ["Whatever", "Whatever"]
+    assert_equal({"Whatever" => [1, 2]}, concordance.ask(lines))
   end
 end
+
+# The source of the kata is the idea of a Concordance, a listing of all the words that appear
+# in a book together with the line numbers that they appear on.
+# More on concordances and their history can be found on Wikipedia here
+#
+# As an example, a concordance of the following text:
+#
+#
+# Now is the winter of our discontent,
+# made glorious summer by a son of York.
+# Would yield the concordance:
+#
+# now: 1
+# is: 1
+# the: 1
+# winter: 1
+# of: 1,2
+# our: 1
+# discontent: 1
+# made: 2
+# glorious: 2
+# summer: 2
+# by: 2
+# a: 2
+# son: 2
+# york: 2
+# The idea is to actually produce a concordance of the Magna Carta text
+# (translation provided into modern English by the British Library).
+# You can find out more about the London Code Dojo at our homepage.
